de83863e73a05cd476a4c7b2e1bdc674c2101d4e
Jerry Yang <jerryyang@JerrydeMacBook-Pro.local>
2019-12-05 15:26:17 -0800

    update ignore file

--- Dota2_trader/decoration_app/views.py
+++ Dota2_trader/decoration_app/views.py
@@ -7,7 +7,7 @@ def index(request):
     latest_item_list = Items.objects.order_by('-pub_date')[:5]
     template = loader.get_template('polls/index.html')
     context = {
-        'latest_question_list': latest_question_list,
+        'latest_item_list': latest_item_list,
     }
     return HttpResponse(template.render(context, request))
 
