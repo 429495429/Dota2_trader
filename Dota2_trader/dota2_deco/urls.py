d7a1d7cf1f361072bbd453631b84d541a82b7222
Jerry Yang <jerryyang@JerrydeMacBook-Pro.local>
2019-12-07 18:13:27 -0800

    dota2_deco:search update 20191207 1813

--- Dota2_trader/dota2_deco/urls.py
+++ Dota2_trader/dota2_deco/urls.py
@@ -5,4 +5,5 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('<int:item_id>/', views.detail, name='detail'),
+    path('<string:keyword',views.search, name='search'),
 ]
--- Dota2_trader/dota2_deco/views.py
+++ Dota2_trader/dota2_deco/views.py
@@ -3,6 +3,7 @@ from django.http import HttpResponse,Http404
 from django.template import loader
 from .models import Decorations, Items
 
+#edit if fulltext search
 def index(request):
     # latest_item_list = Items.objects.order_by('-pub_date')[:5]
     # template = loader.get_template('dota2_deco/index.html')
@@ -12,6 +13,16 @@ def index(request):
     # return HttpResponse(template.render(context, request))
     return HttpResponse("hello")
 
- def detail(request, question_id):
+def detail(request, item_id):
     item = get_object_or_404(Item, pk=item_id)
     return render(request, 'dota2_deco/detail.html', {'item': item, 'decoration': item.item_type})
+
+#edit if keyword search
+def search(request, keyword):
+	# cheapest_item_list = Items.objects.order_by('price')[:-5]
+    # template = loader.get_template('dota2_deco/search.html')
+    # context = {
+    #     'cheapest_item_list': cheapest_item_list,
+    # }
+    # return HttpResponse(template.render(context, request))
+    return HttpResponse("hello")
