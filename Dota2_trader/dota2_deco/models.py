4dfcfa6593a253a89c50037864a6d83eb49ded2c
Jerry Yang <jerryyang@JerrydeMacBook-Pro.local>
2019-12-08 11:51:16 -0800

    indent fix update 20191208

--- Dota2_trader/dota2_deco/models.py
+++ Dota2_trader/dota2_deco/models.py
@@ -2,25 +2,27 @@ import datetime
 from django.db import models
 from django.utils import timezone
 
-# Create your models here.
+# # Create your models here.
 class Decorations(models.Model):
-	decoration_name = models.CharField(max_length=200)
+    
+    decoration_name = models.CharField(max_length=200)
     decoration_champion = models.CharField(max_length=50)
     decoration_part = models.CharField(max_length=50)
     decoration_rarelity = models.CharField(max_length=50)
-    decoration_average_price = models.Double(max=9999.99)
+    decoration_average_price = models.FloatField()
 
     def __str__(self):
-    	return self.decoration_name
+        return self.decoration_name
 
     def recent_average_price(self):
-    	return self.decoration_average_price
+        return self.decoration_average_price
 
 class Items(models.Model):
-    item_id = models.Integer(max_digit=10)
-	item_type = models.ForeignKey(Decorations, on_delete=models.CASCADE)
+    
+    item_id = models.IntegerField()
+    item_type = models.ForeignKey(Decorations, on_delete=models.CASCADE)
     publisher_id = models.CharField(max_length=50)
-    price = models.Double(max=9999.99)
+    price = models.FloatField()
     pub_date = models.DateTimeField('date published')
 
     def __str__(self):
--- Dota2_trader/dota2_deco/views.py
+++ Dota2_trader/dota2_deco/views.py
@@ -5,13 +5,13 @@ from .models import Decorations, Items
 
 #edit if fulltext search
 def index(request):
-    # latest_item_list = Items.objects.order_by('-pub_date')[:5]
-    # template = loader.get_template('dota2_deco/index.html')
-    # context = {
-    #     'latest_item_list': latest_item_list,
-    # }
-    # return HttpResponse(template.render(context, request))
-    return HttpResponse("hello")
+    latest_item_list = Items.objects.order_by('-pub_date')[:5]
+    template = loader.get_template('dota2_deco/index.html')
+    context = {
+        'latest_item_list': latest_item_list,
+    }
+    return HttpResponse(template.render(context, request))
+    # return HttpResponse("hello")
 
 def detail(request, item_id):
     item = get_object_or_404(Item, pk=item_id)
@@ -19,10 +19,10 @@ def detail(request, item_id):
 
 #edit if keyword search
 def search(request, keyword):
-	# cheapest_item_list = Items.objects.order_by('price')[:-5]
-    # template = loader.get_template('dota2_deco/search.html')
-    # context = {
-    #     'cheapest_item_list': cheapest_item_list,
-    # }
-    # return HttpResponse(template.render(context, request))
-    return HttpResponse("hello")
+    cheapest_item_list = Items.objects.order_by('price')[:-5]
+    template = loader.get_template('dota2_deco/search.html')
+    context = {
+        'cheapest_item_list': cheapest_item_list,
+    }
+    return HttpResponse(template.render(context, request))
+    # return HttpResponse("hello")
